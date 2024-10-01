f64efa7e5f19d97da0f37501f0c32a4ff2fa31e3
MohammadAli Pishva <m.a.pishva110@gmail.com>
2024-10-01 16:03:20 +0330

    Optimizing getting image of game card.

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Image, Heading, HStack } from "@chakra-ui/react";
 import { Game } from "./../hooks/useGames";
 import PlatformIconsList from "./PlatformIconsList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface GameProps {
   game: Game;
@@ -10,7 +11,7 @@ interface GameProps {
 const GameCard = ({ game }: GameProps) => {
   return (
     <Card borderRadius="lg" overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between" marginY={2}>
