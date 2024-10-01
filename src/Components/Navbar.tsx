15f9fb12930d4738ba40bce4190a606153ff58fd
MohammadAli Pishva <m.a.pishva110@gmail.com>
2024-10-01 10:14:44 +0330

    Build navbar and add switch mode color.

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,14 @@
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "./../assets/eight-ball.svg";
+import ColorModeSwitch from "./ColorModeSwitch";
+
+const Navbar = () => {
+  return (
+    <HStack justifyContent="space-between" padding="10px">
+      <Image src={logo} boxSize="60px"></Image>
+      <ColorModeSwitch></ColorModeSwitch>
+    </HStack>
+  );
+};
+
+export default Navbar;
