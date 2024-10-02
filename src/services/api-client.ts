a1d48f727ba02688e9fce7d5a57ef8452b618844
MohammadAli Pishva <m.a.pishva110@gmail.com>
2024-10-02 15:55:57 +0330

    Update base url for the 404 error.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https:/api.rawg.io/api",
+    baseURL: "https://api.rawg.io/api",
     params: {
         key: "5b1fc50d25d34f79af178ff2e534439c"
     }
