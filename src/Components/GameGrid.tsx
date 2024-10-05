627eaac1840880b185f196311e78da4a2c52e324
MohammadAli Pishva <m.a.pishva110@gmail.com>
2024-10-05 09:44:33 +0330

    Refactor: use react query for fetching games.

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -13,7 +13,7 @@ const GameGrid = ({ gameQuery }: GameGridProps) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [0, 1, 2, 3, 4, 5, 6, 7];
 
-  if (error) return <Text>{error}</Text>;
+  if (error) return <Text>{error.message}</Text>;
 
   return (
     <SimpleGrid
@@ -28,7 +28,7 @@ const GameGrid = ({ gameQuery }: GameGridProps) => {
           </GameCardContainer>
         ))}
       {!isLoading &&
-        data.map((game) => (
+        data?.results.map((game) => (
           <GameCardContainer key={game.id}>
             <GameCard game={game}></GameCard>
           </GameCardContainer>
